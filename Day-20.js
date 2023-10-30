0c00250206a593a2c54d149777bef5dd6ac45772
Prajwal H C <68982347+prajwalhc-18@users.noreply.github.com>
2023-10-30 12:00:33 +0530

    Add files via upload

---
+++ Day-20.js
@@ -0,0 +1,25 @@
+/* Create two variables num1 and num2 and assign both of the variables a valid number
+
+Create a new variable sum, which holds the sum of num1  and num2
+
+Create a new variable product - this variable should hold the product of num1 and num2
+
+Combine two boolean values flag1 and flag2 and assign a valid boolean value to both of the variables
+
+Create a new variable combinedFlag that holds the logical and value of flag1 and flag2
+ */
+
+const num1 = 5
+const num2 = 10
+
+const sum = num1 + num2
+console.log('Sum:', sum)
+
+const product = num1 * num2
+console.log('Product:', product)
+
+
+const flag1 = true
+const flag2 = false
+const combinedFlag = flag1 && flag2
+console.log('Combined Flag:', combinedFlag)
