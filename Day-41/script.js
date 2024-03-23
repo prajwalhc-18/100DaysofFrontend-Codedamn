bd2aee57d16695c73273492ff45e9633a174bdea
Prajwal H C <68982347+prajwalhc-18@users.noreply.github.com>
2024-03-23 09:58:37 +0530

    Add files via upload

---
+++ Day-41/script.js
@@ -0,0 +1,14 @@
+const num1 = 5
+const num2 = 10
+
+const sum = num1 + num2
+console.log('Sum:', sum)
+
+const product = num1 * num2
+console.log('Product:', product)
+
+
+const flag1 = true
+const flag2 = false
+const combinedFlag = flag1 && flag2
+console.log('Combined Flag:', combinedFlag)
